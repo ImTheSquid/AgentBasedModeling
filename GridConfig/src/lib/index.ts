f11341ba2dc543e8b8d8bc84bcfd695902619f26
Jack Hogan <jackhogan11@gmail.com>
2024-11-14 14:50:27 -0500

    Added grid

--- GridConfig/src/lib/index.ts
+++ GridConfig/src/lib/index.ts
@@ -1,1 +1,40 @@
 // place files you want to import through the `$lib` alias in this folder.
+export enum CellType {
+	Wall,
+	Entrance,
+	Exit,
+	StudyTable,
+	Chair
+}
+
+export interface BaseCell {
+	type: CellType;
+}
+
+export interface EntranceCell extends BaseCell {
+	type: CellType.Entrance;
+	associatedExit: [number, number]; // Coordinate pair for the exit
+}
+
+export interface NonEntranceCell extends BaseCell {
+	type: Exclude<CellType, CellType.Entrance>;
+	associatedExit: null;
+}
+
+// Union type for all cells
+export type Cell = EntranceCell | NonEntranceCell;
+
+export function colorForCell(cell: CellType): string {
+	switch (cell) {
+		case CellType.Wall:
+			return 'red';
+		case CellType.Entrance:
+			return 'blue';
+		case CellType.Exit:
+			return 'green';
+		case CellType.StudyTable:
+			return 'gray';
+		case CellType.Chair:
+			return 'purple';
+	}
+}
--- GridConfig/src/routes/+page.svelte
+++ GridConfig/src/routes/+page.svelte
@@ -1,2 +1,71 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	function createSvg(tagName: string) {
+		var svgNS = 'http://www.w3.org/2000/svg';
+		return document.createElementNS(svgNS, tagName);
+	}
+
+	let grid = function (
+		numberPerSide: number,
+		size: number,
+		pixelsPerSide: number,
+		colors: string[]
+	) {
+		let svg = createSvg('svg');
+		svg.setAttribute('width', pixelsPerSide.toString());
+		svg.setAttribute('height', pixelsPerSide.toString());
+		svg.setAttribute('viewBox', [0, 0, numberPerSide * size, numberPerSide * size].join(' '));
+
+		for (let i = 0; i < numberPerSide; i++) {
+			for (let j = 0; j < numberPerSide; j++) {
+				let color1 = colors[(i + j) % colors.length];
+				let color2 = colors[(i + j + 1) % colors.length];
+				let g = createSvg('g');
+				g.setAttribute('transform', ['translate(', i * size, ',', j * size, ')'].join(''));
+				var number = numberPerSide * i + j;
+				var box = createSvg('rect');
+				box.setAttribute('width', size.toString());
+				box.setAttribute('height', size.toString());
+				box.setAttribute('fill', color1);
+				box.setAttribute('id', 'b' + number);
+				g.appendChild(box);
+				var text = createSvg('text');
+				text.appendChild(document.createTextNode((i * numberPerSide + j).toString()));
+				text.setAttribute('fill', color2);
+				text.setAttribute('font-size', '4');
+				text.setAttribute('x', '0');
+				text.setAttribute('y', (size / 2).toString());
+				text.setAttribute('id', 't' + number);
+				g.appendChild(text);
+				svg.appendChild(g);
+			}
+		}
+		svg.addEventListener(
+			'click',
+			function (e) {
+				let target = e.target as HTMLElement | null;
+				let id = target?.id;
+				if (id) alert(id.substring(1));
+			},
+			false
+		);
+		svg.addEventListener(
+			'mousemove',
+			(e) => {
+				let target = e.target as HTMLElement | null;
+				console.log(target?.id?.substring(1) ?? 'NOID');
+			},
+			false
+		);
+		return svg;
+	};
+
+	onMount(() => {
+		let container = document.getElementById('container') as HTMLElement;
+		container.appendChild(grid(5, 10, 250, ['red', 'white']));
+	});
+</script>
+
+<h1 class="text-xl">HONR 313 Agent-Based Modeling Grid Configurator</h1>
+<div id="container"></div>
